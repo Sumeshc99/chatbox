18ecb6e1c42ccc049635256dd6740b84b4f99a0a
Sumeshc99 <sumeshchaure99@gmail.com>
2023-02-26 12:27:25 +0900

    add files

---
+++ client.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class client implements Runnable{
+
+    private Socket client;
+    private BufferedReader in;
+    private PrintWriter out;
+    private boolean done;
+    
+    @Override
+    public void run(){
+        try{
+            Socket client = new Socket("127.0.0.1",9999);
+            out = new PrintWriter(client.getOutputStream(),true);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        
+            InputHandler inHandler = new InputHandler();
+            Thread t = new Thread(inHandler);
+            t.start();
+
+            String inMassage;
+            while ((inMassage = in.readLine()) != null) {
+                System.out.println(inMassage);
+            }
+        }catch (IOException e) {
+            shutdown();
+        }
+    }
+
+    public void shutdown(){
+        done = true;
+        try {
+            in.close();
+            out.close();
+            if (client.isClosed()) {
+                client.close();
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    class InputHandler implements Runnable{
+
+        @Override
+        public void run(){
+            try{
+                BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+                while (!done) {
+                    String massage = inReader.readLine();
+                    if (massage.equals("/quit")) {
+                        inReader.close();
+                        shutdown();
+                    }else{
+                        out.println(massage);
+                    }
+                }
+            } catch(IOException e){
+                shutdown();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        client client = new client();
+        client.run();
+    }
+}
